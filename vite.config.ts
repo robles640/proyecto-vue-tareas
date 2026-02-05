797a8cb41712881897a02a72e81b65837783f6f3
Alan Felipe Robles Diaz <roblesdiazalan@gmail.com>
2026-02-04 23:45:55 -0400

    fix: base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: "/diplomado-proyecto-vue/"
+  base: "/proyecto-vue-tareas/"
 })
